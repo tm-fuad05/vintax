39eeb7b01a4dbc9e8a7a4a3c0cbc75a62846d6b4
tm-fuad05 <tm.fuad05@gmail.com>
2026-03-03 06:34:27 +0600

    fix

--- src/component/shared/logo.tsx
+++ src/component/shared/logo.tsx
@@ -9,7 +9,7 @@ export default function Logo() {
       <Link href="/" className="flex items-center gap-1 group px-2">
         <FaTshirt className="text-primary rotate-10 text-xl md:text-2xl lg:text-[32px]" />
         <h2 className="text-2xl md:text-3xl font-black text-title italic">
-          VANTIX
+          VINTAX
         </h2>
       </Link>
     </div>
